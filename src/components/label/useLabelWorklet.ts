1f866c9519b195f47770571c1bf2228f6d4ab79b
Dmytro Melnyk <melnykdmytro26@gmail.com>
2026-03-09 18:06:08 +0100

    fix: added new logic with animation for LabelWorklet

--- src/components/label/useLabelWorklet.ts
+++ src/components/label/useLabelWorklet.ts
@@ -1,11 +1,19 @@
-import { useAnimatedProps, useDerivedValue } from 'react-native-reanimated';
+import {
+  useAnimatedProps,
+  useDerivedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import { useRotaryTimer } from '../../hooks';
 
 export const useLabelWorklet = () => {
   const { rotationSharedValue, renderLabel } = useRotaryTimer();
 
+  const animatedRotationSharedValue = useDerivedValue(() => {
+    return withTiming(rotationSharedValue.value);
+  });
+
   const textSharedValue = useDerivedValue(() => {
-    return renderLabel?.(rotationSharedValue.value) || '';
+    return renderLabel?.(animatedRotationSharedValue.value) || '';
   });
 
   const animatedProps = useAnimatedProps(() => {
